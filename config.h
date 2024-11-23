64710e574aa225a65d68f0d853eaa2acc665311b
aeinstein <mb@tellimate.de>
2024-11-23 17:32:52 +0100

    refactored

---
+++ config.h
@@ -0,0 +1,28 @@
+#define Left_IN A1
+#define Right_IN A2
+
+#define LED_DATA_LEFT 1
+#define LED_DATA_RIGHT 2
+
+#define NUM_LEDS 28
+
+// 10%
+#define BRIGHTNESS 0.1f
+
+// idle color
+#define GLOWNESS 0
+
+// Use Stereo input. If Mono then only left channel is used
+#define STEREO
+
+// Number of analog probes
+#define NUMREADS 10
+
+// Peek, default decay 0.03f
+#define PEAK_INDICATOR
+#define INDICATOR_STYLE 1
+#define PEAK_DECAY 0.1f
+
+// delay per loop, default 5ms
+#define DELAY_MS 10
+
